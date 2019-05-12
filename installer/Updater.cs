bc4229c6c304478aa7863e4bcbee6df3de6368f9
Nick McClorey <mcclorns@mail.uc.edu>
2019-05-11 21:12:51 -0400

    created install directory

---
+++ installer/Updater.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+
+namespace tropic_updater
+{
+    class Updater
+    {
+        private const string remoteUrl = "https://raw.githubusercontent.com/nickrmcclorey/tropic-bin/master/";
+        private static string latestVersion = "";
+
+        static void Main(string[] args)
+        {
+            latestVersion = GetRequest("latest_version.txt");
+            string currentVersion = File.ReadAllText("installed_version.txt");
+            if (latestVersion == currentVersion)
+            {
+                Console.WriteLine("Latest version of Tropic file browser is installed");
+                return;
+            }
+
+            Console.WriteLine("Updating Tropic from " + currentVersion + " to " + latestVersion);
+            DownloadFile("tropic-binaries/hashes.json");
+            Console.WriteLine("Calculating Hashes");
+            Dictionary<string, string> latestHashes = JsonConvert.DeserializeObject<Dictionary<string, string>>(GetRequest("tropic-binaries/hashes.json"));
+            List<string> filesToUpdate = filesToDownload(latestHashes);
+            Console.WriteLine("Downloading " + filesToUpdate.Count + " file(s)");
+            DownloadFiles(filesToUpdate);
+            Console.WriteLine("Installing files");
+            InstallFiles(filesToUpdate);
+            Directory.Delete("./web_downloads", true);
+            File.WriteAllText("installed_version.txt", latestVersion);
+            Console.WriteLine("downloaded and installed " + filesToUpdate.Count + " file(s)");
+        }
+
+        private static void InstallFiles(List<string> filesToUpdate)
+        {
+            foreach (string file in filesToUpdate)
+            {
+                Directory.CreateDirectory("./tropic-win32-x64/" + file.Substring(0, file.LastIndexOf("\\")));
+                File.Copy("web_downloads/" + file, "./tropic-win32-x64/" + file, true);
+            }
+        }
+        
+        private static void DownloadFiles(List<string> files)
+        {
+            using (WebClient myWebClient = new WebClient())
+            {
+                foreach (string file in files)
+                {
+                    string myStringWebResource = remoteUrl + "tropic-binaries/tropic-win32-x64/" + file;
+                    myStringWebResource = myStringWebResource.Replace("\\", "/").Replace("#", "%23");
+                    try
+                    {
+                        string localPath = "web_downloads/" + file.Replace("\\", "/");
+                        string fileName = Path.GetFileName(localPath);
+                        Directory.CreateDirectory(localPath.Substring(0, localPath.LastIndexOf("/")));
+                        myWebClient.DownloadFile(myStringWebResource, localPath);
+                    }
+                    catch (System.Net.WebException)
+                    {
+                        Console.WriteLine("failed to download " + myStringWebResource);
+                        Environment.Exit(1);
+                    }
+                }
+            }
+        }
+
+        private static void DownloadFile(string filePath, string localPath = null)
+        {
+            using (WebClient myWebClient = new WebClient())
+            {
+                string myStringWebResource = remoteUrl + filePath;
+                localPath = localPath ?? "./web_downloads/" + filePath;
+                string fileName = Path.GetFileName(localPath);
+                // create a directory, but chop of the filename from the path passed in
+                Directory.CreateDirectory(localPath.Replace(fileName, ""));
+                myWebClient.DownloadFile(myStringWebResource, localPath);
+            }
+        }
+
+        private static string GetRequest(string filePath)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                string webResource = remoteUrl + filePath;
+                return webClient.DownloadString(webResource);
+            }
+        }
+
+        private static List<string> filesToDownload(Dictionary<string, string> hashes)
+        {
+            List<string> modifiedFiles = new List<string>();
+            foreach (KeyValuePair<string, string> file in hashes)
+            {
+                if (ComputeShaHash(file.Key) != file.Value)
+                {
+                    modifiedFiles.Add(file.Key);
+                }
+            }
+
+            return modifiedFiles;
+        }
+
+        private static string ComputeShaHash(string path)
+        {
+            string filePath = Path.Combine("./tropic-win32-x64", path);
+            if (!File.Exists(filePath))
+            {
+                return null;
+            }
+
+            using (var sha = SHA256.Create())
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    return Convert.ToBase64String(sha.ComputeHash(stream));
+                }
+            }
+        }
+    }
+}
---
+++ installer/tropic-installer.nsi
@@ -0,0 +1,73 @@
+; The name of the installer
+Name "Tropic-Installer"
+
+; The file to write
+OutFile "tropicInstaller.exe"
+
+; The default installation directory
+InstallDir $PROGRAMFILES\tropic
+
+; Registry key to check for directory (so if you install again, it will 
+; overwrite the old one automatically)
+InstallDirRegKey HKLM "Software\NSIS_Example2" "Install_Dir"
+
+; Request application privileges for Windows Vista
+RequestExecutionLevel admin
+
+;--------------------------------
+
+; Pages
+
+Page components
+Page directory
+Page instfiles
+
+UninstPage uninstConfirm
+UninstPage instfiles
+
+;--------------------------------
+
+; The stuff to install
+Section "Tropic-Installer (required)"
+
+  SectionIn RO
+  
+  ; Set output path to the installation directory.
+  SetOutPath $INSTDIR
+  
+  ; Put file there
+  File /r ".\tropic-win32-x64"
+  File ".\installed_version.txt" 
+  File ".\Newtonsoft.Json.dll"
+  File ".\Updater.exe"
+  
+  ; Write the installation path into the registry
+  WriteRegStr HKLM SOFTWARE\NSIS_Example2 "Install_Dir" "$INSTDIR"
+  
+  ; Write the uninstall keys for Windows
+  WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\Example2" "DisplayName" "Tropic File Browser"
+  WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\Example2" "UninstallString" '"$INSTDIR\uninstall.exe"'
+  WriteRegDWORD HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\Example2" "NoModify" 1
+  WriteRegDWORD HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\Example2" "NoRepair" 1
+  WriteUninstaller "$INSTDIR\uninstall.exe"
+  
+SectionEnd
+
+; Uninstaller
+Section "Uninstall"
+  
+  ; Remove registry keys
+  DeleteRegKey HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\Example2"
+  DeleteRegKey HKLM SOFTWARE\NSIS_Example2
+
+  ; Remove files and uninstaller
+  Delete "$INSTDIR\tropic-win32-x64\*"
+  Delete "$INSTDIR\web_downloads\*"
+  Delete "$INSTDIR\Updater.exe"
+  Delete "$INSTDIR\installed_version.txt"
+  Delete "$INSTDIR\Newtonsoft.Json.dll"
+  Delete "$INSTDIR\uninstall.exe"
+  RMDir /r "$INSTDIR\tropic-win32-x64"
+  RMDir /r "$INSTDIR\web_downloads"
+
+SectionEnd
